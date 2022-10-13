40132b9bf4693864dc6d602b5d1340ea7b973054
Aditi Jain <100277545+jaditi930@users.noreply.github.com>
2022-10-14 03:08:38 +0530

    download qr code

--- qrs.py
+++ qrs.py
@@ -1,7 +1,10 @@
+from tkinter import filedialog
 import pyqrcode
 import os
 from tkinter import *
 from PIL import Image,ImageTk
+qr_path=os.getcwd()+"\myqr.png"
+
 def getVals():
     global s
     s=stringurl.get()
@@ -9,20 +12,28 @@ def getVals():
     ur.png("myqr.png", scale = 6)
     qr_path=os.getcwd()+"\myqr.png"
     image.config(file=f"{qr_path}")
-    
+
+def dwld():
+    folder=filedialog.askdirectory()
+    cur_path=os.getcwd()
+    os.chdir(folder)
+    qr=Image.open(f"{qr_path}") 
+    qr=qr.save("qrimage.jpg")
+    os.chdir(cur_path)
 window=Tk()
 window.geometry("644x434")
 window.title("QR Generator")
-text=Label(text="Enter URL to generate QR Code",font="comicsansms 19 bold")
+text=Label(text="Enter URL to generate QR Code",font="comicsansms 10 bold")
 stringurl=StringVar()
 url=Entry(textvariable=stringurl)
 submit=Button(text="Submit",command=getVals)
-qr_path=os.getcwd()+"\myqr.png"
 image=PhotoImage(file="")
 photo=Label(image=image)
-text.grid()
-url.grid()
-submit.grid(row=2,column=0)
+text.grid(row=3,column=2)
+url.grid(row=5,column=2)
+submit.grid(row=10,column=2)
 photo.grid()
+download=Button(text="Save QR Code",command=dwld)
+download.grid()
 window.mainloop()
 
